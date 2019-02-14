0826d36ce6700af6cedfccd345e22562bd8af5e9
Andi Herlan <herlandi.27@gmail.com>
2019-02-14 22:00:08 +0700

    merapikan data hujan jadi dua kolom tgl dan prc

--- GRDC_plot.R
+++
@@ -1,9 +0,0 @@
-# Filter GRDC catalogue based on a country code
-GRDC_IT <- catalogueGRDC(columnName = "country_code", columnValue = "IT")
-
-# Convert the table to a SpatialPointsDataFrame
-hydro <- SpatialPointsDataFrame(coords = GRDC_IT[, c("long", "lat")], data = GRDC_IT)
-
-# Plot the stations on the map
-plot(Italy, col = NA, border = "darkgrey")
-plot(hydro, add = TRUE, col = "blue", pch = 1)
--- indonesia.R
+++
@@ -1,4 +0,0 @@
-# indahnya ibu pertiwi, mun anak si teteh mah pratiwi
-library(raster)
-indonesia <- getData("GADM", country = "IDN", level = 0)
-plot(indonesia, col = NA, border = "darkgrey")
